aa7a4c9f0a0612e79c5f2b246ea026102839fa83
dkovalenko1 <dkovalenko1@kse.org.ua>
2025-04-13 17:55:15 +0300

    added testing and saving to csv file

---
+++ Algorithm/Classes/ExperimentResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithm.Classes;
+
+public class ExperimentResult
+{
+    public required int VertexCount { get; set; }
+    public required double EdgeDensity { get; set; }
+    public required string Representation { get; set; }
+    public required double Time { get; set; }
+    
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,5 @@
-﻿using Algorithm.Utilities;
+﻿using System.Diagnostics;
+using Algorithm.Utilities;
 
 namespace Algorithm;
 using Classes;
@@ -6,33 +7,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int vertices = 20;
-        double density = 1;
-        GraphGenerator generator = new GraphGenerator();
-        var edges = generator.GenerateGraph(vertices, density);
-        
-        
-        var listGenerator = new ListGraph(vertices);
-        foreach (var (from, to) in edges)
-        {
-            listGenerator.AddEdge(from, to);
-        }
-        listGenerator.Print();
-        var listGraph = listGenerator.GetAdjList();
-        var sorter = new TopologicalSort(listGraph);
-        var result = sorter.Sort();
-        foreach (var i in result)
-        {
-            Console.Write(i + "->");
-        }
-        
-        
-        var matrixGraph = new MatrixGraph(vertices);
-        foreach (var (from, to) in edges)
-        {
-            matrixGraph.AddEdge(from, to);
-        }
-        
+        Testing.Test();
     }
 }
 
